5f58de79395c02a59682d75b50ee4ff67f9b60ce
Eight Jq <mishakoposov@mail.ru>
2020-04-03 17:11:50 +0300

    Добавьте файлы проекта.

---
+++ InheritanceExample (Ex2)/InheritanceExample (Ex2).cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class GameObject
+{
+private:
+	string name;
+protected:
+	GameObject() : name("") {};
+	GameObject(const GameObject& object) : name(object.name) {}
+	GameObject(const string& _name) :name(_name) {}
+	virtual ~GameObject() { cout << "Game object has been deleted" << endl; }
+	virtual void  _print() const { cout << endl << "Name: " << name; }
+public:
+	void  print() const
+	{
+		 cout << typeid(*this).name() << ": ";
+		_print(); cout << endl;
+	}
+	virtual void draw() = 0; // Чистая виртуальная функция
+};
+
+class Сharacters :public GameObject
+{
+private:
+	int healthpoints;
+	int manapoints;
+	int X;
+	int Y;
+public:
+	Сharacters() :GameObject() {};
+	Сharacters(const string& _name, int _healthpoints, int _manapoints) : GameObject(_name), healthpoints(_healthpoints), manapoints(_manapoints),  X(0), Y(0) {}
+	Сharacters(const Сharacters& obj) : GameObject(obj), healthpoints(obj.healthpoints), manapoints(obj.manapoints),  X(obj.X), Y(obj.Y) {}
+	Сharacters(const string& _name, int _healthpoints, int _manapoints,  int _X, int _moveY) : GameObject(_name), healthpoints(_healthpoints), manapoints(_manapoints),  X(_X), Y(_moveY) {}
+	void _move(int x, int y)
+	{
+		X += x;
+		Y += y;
+	} 
+	int getX()  { return X; }
+	int getY()  { return Y;}
+	~Сharacters() { cout << "Character has been deleted" << endl; }
+protected:
+	virtual void move(int x, int y) = 0; // Чистая виртуальная функция
+	void  _print() const override
+	{
+		GameObject::_print();
+		cout << "\nHealthpoints: " << healthpoints << "\nManapoints: " << manapoints <<endl<<"x position: " << X <<endl<<"y position: " << Y;
+	}
+};
+
+class MythCreation :public Сharacters
+{
+private:
+	string type;
+public:
+	MythCreation() : Сharacters() {};
+	MythCreation(const string& _name, int _healthpoints, int _manapoints,  string _type) :Сharacters(_name, _healthpoints, _manapoints), type(_type) {}
+	MythCreation(const MythCreation& obj) : Сharacters(obj), type(obj.type) {}
+	MythCreation(const string& _name, int _healthpoints, int _manapoints,  string _type, int _X, int _moveY) :
+	Сharacters(_name, _healthpoints, _manapoints,  _X, _moveY), type(_type) {}
+	~MythCreation() { cout << "MythCreation has been deleted" << endl; }
+	void move(int x, int y) override
+	{
+		Сharacters::_move(x, y);
+		cout << "MythCreation crossed to the coordinates: " << getX() << "," << getY() << endl;
+	}
+	void draw()
+	{
+		cout << "Position of MythCreation: " << getX() << "," << getY() << endl;
+	}
+protected:
+	void _print()const override
+	{
+		Сharacters::_print();
+		cout <<endl<<"MythCreation type: " << type;
+	}
+};
+
+class HumanCharacter :public Сharacters
+{
+private:
+	string weapon;
+	int level;
+public:
+	HumanCharacter() : Сharacters() {};
+	HumanCharacter(const string& _name, int _healthpoints, int _manapoints,  string _weapon, int _level) :Сharacters(_name, _healthpoints, _manapoints), weapon(_weapon), level(_level) {}
+	HumanCharacter(const HumanCharacter& obj) : Сharacters(obj), weapon(obj.weapon), level(obj.level) {}
+	HumanCharacter(const string& _name, int _healthpoints, int _manapoints, int X, int moveY, string _weapon, int _level) :
+	Сharacters(_name, _healthpoints, _manapoints), weapon(_weapon), level(_level) {}
+	~HumanCharacter() { cout << "Human Character has been deleted" << endl; }
+	void move(int x, int y) override
+	{
+		Сharacters::_move(x, y);
+		cout << "Human character crossed to the coordinates: " << getX() << "," << getY() << endl;
+	}
+	void draw() override { cout << "Human character position: " << getX() << "," << getY() << endl; }	
+protected:
+
+	void _print() const override
+	{
+		Сharacters::_print();
+		cout <<endl<<"Human weapon: " << weapon <<endl<<"level: " << level;
+	}
+};
+
+class MagicCharacter :public Сharacters
+{
+private:
+	string nameMagic;
+public:
+	MagicCharacter() :Сharacters() {};
+	MagicCharacter(const string& _name, int _healthpoints, int _manapoints, string _nameMagic) :Сharacters(_name, _healthpoints, _manapoints), nameMagic(_nameMagic) {}
+	MagicCharacter(const MagicCharacter& obj) : Сharacters(obj), nameMagic(obj.nameMagic) {}
+	MagicCharacter(const string& _name, int _healthpoints, int _manapoints,  int _X, int _moveY, string _nameMagic) :
+	Сharacters(_name, _healthpoints, _manapoints, _X, _moveY), nameMagic(_nameMagic) {}
+	~MagicCharacter() { cout << "Magic Character has been deleted" << endl; }
+	void move(int x, int y) override
+	{
+		Сharacters::_move(x, y);
+		cout << "Magic character crossed to the coordinates: " << getX() << "," << getY() << endl;
+	}
+	void draw() override { cout << "Magic Character position: " << getX() << "," << getY() << endl; }
+protected:
+
+	void _print()const override
+	{
+		Сharacters::_print();
+		cout <<endl<<"Magic Character: " << nameMagic;
+	}
+};
+
+class Weapon :public GameObject
+{
+private:
+	int damage;
+public:
+	Weapon() : GameObject() {};
+	Weapon(const string& _name, int _damage) :GameObject(_name), damage(_damage) {}
+	Weapon(const Weapon& obj) :GameObject(obj), damage(obj.damage) {}
+	~Weapon() { cout << "Weapon has been deleted" << endl; }
+protected:
+	void draw() override { cout << "Character with next weapon: "; }
+	void _print()const override
+	{
+		GameObject::_print();
+		cout <<endl<<"Damage: " << damage ;
+	}
+};
+
+int main()
+{
+	MythCreation merveil("Merveil, the Siren", 200, 500, "Siren");
+	MagicCharacter sin("Sin, Thief of Virtue", 150, 100, "None");
+	Weapon lakishu("Lakishu's Blade", 15);
+	merveil.draw();	
+	merveil.move(20, 50);
+	merveil.print(); cout << endl;
+	sin.draw();
+	sin.move(10, 35);
+	sin.print(); cout << endl;
+	lakishu.print();
+}
---
+++ InheritanceExample/InheritanceExample.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+class B1 {
+	int a;
+public:
+	B1() { cout << "object of " << typeid(*this).name() << " was created" << endl; }
+	B1(int x) :B1() { a = x; }
+	~B1() { cout << "object of " << typeid(*this).name() << " was destroyed" << endl; }
+};
+class D1 : public B1 {
+	int b;
+public:
+	D1(int x, int y) : B1(y)
+	{
+		b = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	};
+	~D1() { cout << "object of " << typeid(*this).name() << " was destroyed" << endl; }
+};
+class D2 : private B1 {
+	int c;
+public:D2(int x, int y) :B1(y)
+{
+	c = x;
+	cout << "object of " << typeid(*this).name() << " was created\n";
+}
+	  ~D2() { cout << "object of " << typeid(*this).name() << " was destroyed" << endl; }
+};
+class B2 {
+	int d;
+public:
+	B2() { cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B2(int z)
+	{
+		d = z;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~B2() { cout << "object of " << typeid(*this).name() << " was destroyed" << endl; }
+};
+class D4 : public D1, public D2, public B2 {
+	int e;
+public:
+	D4(int x, int y, int z, int i, int j, int k) :D1(y, z), D2(i, j), B2(k)
+	{
+		e = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D4() { cout << "object of " << typeid(*this).name() << " was destroyed" << endl; }
+};
+int main()
+{
+	D4(1, 2, 3, 4, 5, 6);
+}
+
